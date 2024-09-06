c6b3c0ff00b36c2e1be2abf8ec141b0ce13f3aa4
maxigonza10 <maximo401777@gmail.com>
2024-09-06 16:26:26 -0300

    se termino la modularizacion del repositorio backend-jwt

--- backend-jwt/src/config/config.js
+++ backend-jwt/src/config/config.js
@@ -2,8 +2,9 @@ export const valoresBD={
     DB_HOST: 'localhost',
     DB_USER: 'root',
     DB_PASSWORD: '',
-    DB_NAME: 'db_system',
-    PORT:4000,
-    SECRET_KEY:'aopz9-3f1iw12-4ja94f-6l4jzg8',
-    SECRET_SESSION:"session_secret"
+    DB_NAME: 'db_system'
 }
+
+export const PORT = 4000
+export const SECRET_KEY ='aopz9-3f1iw12-4ja94f-6l4jzg8'
+export const SECRET_SESSION = "session_secret"
